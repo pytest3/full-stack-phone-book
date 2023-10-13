1f264307d9fe720ce77b868d0230f85026030d23
z <prospectheory@gmail.com>
2023-10-14 00:48:42 +0800

    include morgan middleware and change folder structure

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,31 @@ const express = require("express");
 let persons = require("./src/models/data.js");
 const fs = require("fs");
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+morgan.token("body", function getBody(req) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens.body(req, res),
+    ].join(" ");
+  })
+);
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
