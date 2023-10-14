be469d2787ffd0355bbaacdc33767644aa25d127
z <prospectheory@gmail.com>
2023-10-15 00:49:14 +0800

    bug: use node instead of nodemon for start script. this was causing fly.io to crash

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,11 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("dist"));
 
 morgan.token("body", function getBody(req) {
   return JSON.stringify(req.body);
