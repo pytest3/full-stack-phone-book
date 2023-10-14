24276bde58e7d5cdc128a7f4c01549a7dc16af89
z <prospectheory@gmail.com>
2023-10-14 15:06:56 +0800

    feat: add contact form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import ContactForm from "./components/ContactForm";
 import PhoneBook from "./components/PhoneBook";
 
 function App() {
   return (
     <>
       <div>Home page</div>
+      <ContactForm />
       <PhoneBook />
     </>
   );
--- frontend/src/components/ContactForm/ContactForm.jsx
+++ frontend/src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,39 @@
 import React from "react";
 
 export default function ContactForm() {
-  return <div>ContactForm</div>;
+  const [inputs, setInputs] = React.useState({ name: "", number: "" });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    window.alert(JSON.stringify(inputs, null, 4));
+    setInputs({ name: "", number: "" });
+  }
+
+  function handleChange(e) {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Save a new contact</h1>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={inputs.name}
+        onChange={handleChange}
+      ></input>
+      <label htmlFor="number">Number</label>
+      <input
+        type="number"
+        id="number"
+        name="number"
+        value={inputs.number}
+        onChange={handleChange}
+      ></input>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
+globalThis;
