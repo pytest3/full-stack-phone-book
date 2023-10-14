385177c4e5c70aef1d7ee0a61fdb8546fa18beab
z <prospectheory@gmail.com>
2023-10-15 00:55:08 +0800

    change URL to relative path

--- frontend/src/components/PhoneBook/PhoneBook.jsx
+++ frontend/src/components/PhoneBook/PhoneBook.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 
-const URL = "http://localhost:3000/api/persons";
+// const URL = "http://localhost:3000/api/persons";
+const baseURL = "/api/persons";
 
 export default function PhoneBook() {
   const [persons, setPerson] = React.useState([
     { id: 83291831938, name: "charlee", number: "98776588" },
   ]);
+
   React.useEffect(() => {
     async function fetchData() {
-      const response = await fetch(URL);
+      const response = await fetch(baseURL);
       const data = await response.json();
       setPerson(data);
     }
     fetchData();
   }, []);
+
   return (
     <div>
-      <h1>PhoneBook!</h1>
+      <h1>PhoneBook!!</h1>
       {persons.map((i) => (
         <div key={i.id}>{i.name}</div>
       ))}
