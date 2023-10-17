28f9a12ebba6725189c73426fa670b11627b5613
z <prospectheory@gmail.com>
2023-10-17 22:43:37 +0800

    conneted express with postgres db using node-postgres

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,21 @@ const morgan = require("morgan");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const pg = require("pg");
+const { Pool } = pg;
+// set the way we will connect to the server
+const pgConnectionConfigs = {
+  user: "z",
+  host: "localhost",
+  database: "z",
+  port: 5432, // Postgres server always runs on this port
+};
+// create new pool instance which create new clients lazily when needed
+// pool ensures that these new clients connect automatically to server
+// pool will dispatch every query passed to pool.query on the 1st avail idle client
+// by default there's a max of 10 clients per pool
+const pool = new Pool(pgConnectionConfigs);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
@@ -28,32 +43,47 @@ app.use(
     ].join(" ");
   })
 );
+
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("hi");
 });
 
 app.get("/api/persons", (req, res) => {
-  res.send(JSON.stringify(persons, null, 4));
+  function queryCallback(error, result) {
+    if (error) {
+      console.log("Error executing query", error.stack);
+      res.status(503).send(result.rows || "error");
+    }
+    res.send(JSON.stringify(result, null, 4));
+    console.log(result.rows);
+  }
+  pool.query("SELECT * FROM students", queryCallback);
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  const person = persons.find((i) => i.id === Number(id));
-  if (!person) {
-    res.status(404).send("Error: person not found");
-  }
-  res.send(person);
-});
+// app.get("/api/persons/:id", (req, res) => {
+//   const id = req.params.id;
+//   const person = persons.find((i) => i.id === Number(id));
+//   if (!person) {
+//     res.status(404).send("Error: person not found");
+//   }
+//   res.send(person);
+// });
 
 app.delete("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  persons = persons.filter((i) => {
-    return i.id !== Number(id);
-  });
-  res.status(204).end();
+  const toBeDeletedId = [id];
+  const sqlText = "DELETE FROM students WHERE id = $1";
+  function handleDeletion(err, result) {
+    if (err) {
+      res.status(503).end();
+      console.log("Error executing delete request");
+    }
+    res.send("Delete success");
+  }
+  pool.query(sqlText, toBeDeletedId, handleDeletion);
 });
 
 app.get("/info", (req, res) => {
@@ -64,22 +94,86 @@ app.get("/info", (req, res) => {
   );
 });
 
-app.post("/api/persons", (req, res) => {
-  const { name, number } = req.body;
+// app.post("/api/persons", (req, res) => {
+//   const { firstName, lastName, mobile, gender } = req.body;
 
-  if (!name) {
-    res.status(404).send({ error: "name must be present" });
-  }
-  if (!number) {
-    res.status(404).send({ error: "number must be present" });
-  }
-  if (persons.find((i) => i.name === name)) {
-    res.status(404).send({ error: "name must be unique" });
+//   if (!name) {
+//     res.status(404).send({ error: "name must be present" });
+//   }
+//   if (!number) {
+//     res.status(404).send({ error: "number must be present" });
+//   }
+//   if (persons.find((i) => i.name === name)) {
+//     res.status(404).send({ error: "name must be unique" });
+//   }
+
+//   const values = [firstName, lastName, mobile, gender];
+//   const sqlText =
+//     "INSERT INTO students(first_name, last_name, mobile, gender) VALUES ($1, $2, $3, $4)";
+
+//   function handleAddPerson(err, result) {
+//     if (err) {
+//       console.log(err);
+//       res.status(503).end();
+//     }
+
+//     res.send(result.rows);
+//   }
+
+//   pool.request(sqlText, values, handleAddPerson);
+
+//   const newPerson = { id: crypto.randomUUID(), ...req.body };
+//   persons = [...persons, newPerson];
+//   res.end();
+// });
+
+app.put("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const { lastName, firstName } = req.body;
+
+  const values = [id, firstName, lastName];
+  const sqlText =
+    "UPDATE students SET first_name = $2, last_name = $3 WHERE id = $1";
+  function handleEdit(err, result) {
+    if (err) {
+      console.log(err);
+      res.status(500).end();
+    }
+    res.send(`Updated id: ${id}`);
   }
 
-  const newPerson = { id: crypto.randomUUID(), ...req.body };
-  persons = [...persons, newPerson];
-  res.end();
+  pool.query(sqlText, values, handleEdit);
 });
 
 app.listen(port, () => console.log("listening on port 3000"));
+
+// --- NODE PG --- /////////////////////////////////////////////
+
+// const { Client } = pg;
+
+// create a client instance
+// const client = new Client(pgConnectionConfigs);
+
+// make the connection to the server
+// client.connect();
+
+// create the query done callback
+// const whenQueryDone = (error, result) => {
+//   if (error) {
+//     console.log("error", error);
+//   } else {
+//     console.log(result.rows); // rows key has the data
+//   }
+//   client.end(); // close the connection
+// };
+
+// const inputData = ["lord", "slayer", 938232488, "t"];
+// write the SQL query
+// const sqlQuery = "SELECT * FROM students";
+// const sqlQuery =
+//   "INSERT INTO students(first_name, last_name, mobile, gender) VALUES($1, $2, $3, $4) RETURNING *";
+
+// run the SQL query
+// client.query(sqlQuery, inputData, whenQueryDone);
+
+// --- NODE PG --- /////////////////////////////////////////////
